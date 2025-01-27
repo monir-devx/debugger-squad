932502dc68809c682f24060f0c5ffafdd53303bc
monirjihad <monirhossainjihad@gmail.com>
2025-01-28 01:54:02 +0600

    Seed initial data for Category table

--- Ecommerce/EcommerceWeb/Data/ApplicationDbContext.cs
+++ Ecommerce/EcommerceWeb/Data/ApplicationDbContext.cs
@@ -9,5 +9,13 @@ namespace EcommerceWeb.Data
         {
         }
         public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //Overriding given function
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
     }
 }
