5f487fc389c9fe2768ea77dfa0bb84b4c1b30717
monirjihad <monirhossainjihad@gmail.com>
2025-01-28 22:40:00 +0600

    Display all Categories on the Index view page

--- Ecommerce/EcommerceWeb/Controllers/CategoryController.cs
+++ Ecommerce/EcommerceWeb/Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace EcommerceWeb.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
