712a334ff79cd49102806826094f9dd9440753fc
monirjihad <monirhossainjihad@gmail.com>
2025-03-18 20:38:00 +0600

    Implement ICategoryRepository

--- Ecommerce/Ecommerce.DataAccess/Repository/IRepository/ICategoryRepository.cs
+++ Ecommerce/Ecommerce.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bulky.DataAccess.Repository.IRepository
+namespace Ecommerce.DataAccess.Repository.IRepository
 {
     public interface ICategoryRepository : IRepository<Category>
     {
