a1aac2f34a7e798f0720efefb6ff10f27dc22ffb
NasifUzZaman38 <nasif.uzzaman.38@gmail.com>
2025-04-22 12:09:45 +0600

    Enhance ShoppingCartVM with OrderHeader to support dynamic order total calculation

--- Ecommerce/Ecommerce.Models/ViewModels/ShoppingCartVM.cs
+++ Ecommerce/Ecommerce.Models/ViewModels/ShoppingCartVM.cs
@@ -10,7 +10,7 @@ namespace Ecommerce.Models.ViewModels
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
 
-        public double OrderTotal { get; set; }
     }
 }
