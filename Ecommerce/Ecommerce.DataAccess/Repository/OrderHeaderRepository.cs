a93480a765dffbbf4eab7b85f45c1227099e1452
NasifUzZaman38 <nasif.uzzaman.38@gmail.com>
2025-04-22 12:00:48 +0600

    Add OrderHeader and OrderDetail repositories and register them in UnitOfWork

---
+++ Ecommerce/Ecommerce.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.DataAccess.Data;
+using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
