bbd6295c84a5bae46bbf431d973e97333e0c00d3
NasifUzZaman38 <nasif.uzzaman.38@gmail.com>
2025-04-29 02:43:53 +0600

    Fix cart session count bug by correctly removing cart before updating session; remove unnecessary entity tracking during deletion.

--- Ecommerce/EcommerceWeb/Areas/Customer/Controllers/CartController.cs
+++ Ecommerce/EcommerceWeb/Areas/Customer/Controllers/CartController.cs
@@ -214,13 +214,13 @@ namespace EcommerceWeb.Areas.Customer.Controllers
 
         public IActionResult Minus(int cartId)
         {           
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
             if (cartFromDb.Count <= 1)
             {
                 //remove that from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
                 HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
                     .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
-                _unitOfWork.ShoppingCart.Remove(cartFromDb);
             }
             else
             {
@@ -234,10 +234,10 @@ namespace EcommerceWeb.Areas.Customer.Controllers
 
         public IActionResult Remove(int cartId)
         {
-            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId, tracked: true);
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
             HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart
                 .GetAll(u => u.ApplicationUserId == cartFromDb.ApplicationUserId).Count() - 1);
-            _unitOfWork.ShoppingCart.Remove(cartFromDb);
             _unitOfWork.Save();
             return RedirectToAction(nameof(Index));
         }
