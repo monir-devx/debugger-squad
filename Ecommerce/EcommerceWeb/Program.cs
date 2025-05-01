fc6d11ad7945793254b37ae6027e4e1a628206d2
monirjihad <monirhossainjihad@gmail.com>
2025-05-01 12:33:41 +0600

    update connection string

--- Ecommerce/EcommerceWeb/Program.cs
+++ Ecommerce/EcommerceWeb/Program.cs
@@ -23,7 +23,7 @@ builder.Configuration
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
 
